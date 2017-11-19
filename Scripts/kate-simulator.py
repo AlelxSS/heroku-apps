bcafdaf762fc42a189b57a3766af637580ac1f52
AlelxSS <titorovichva@gmail.com>
2017-11-19 09:04:14 +0300

    kate cimulator

--- Scripts/kate-simulator.py
+++ Scripts/kate-simulator.py
@@ -5,8 +5,8 @@ token = '497028477:AAERLo-AfA-NwqvcngZtuM5BvUq-wxVCLOM'
 bot = telebot.TeleBot(token)
 
 @bot.message_handler(content_types=["text"])
-def repeat_all_messages(message): # Название функции не играет никакой роли, в принципе
-    bot.send_message(message.chat.id, message.text)
+def answer_all_messages(message): # Название функции не играет никакой роли, в принципе
+    bot.send_message('hello'.chat.id, message.text)
 
 if __name__ == '__main__':
     bot.polling(none_stop=True)
