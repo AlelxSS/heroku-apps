1941e5fabe6be2603ebab183b390e2c54ecf73eb
AlelxSS <titorovichva@gmail.com>
2017-11-19 09:16:50 +0300

    kate cimulator

--- Scripts/kate-simulator.py
+++ Scripts/kate-simulator.py
@@ -4,9 +4,11 @@ token = '497028477:AAERLo-AfA-NwqvcngZtuM5BvUq-wxVCLOM'
 
 bot = telebot.TeleBot(token)
 
-@bot.message_handler(content_types=["text"])
-def answer_all_messages(message): # Название функции не играет никакой роли, в принципе
-    bot.send_message(message.chat.id, 'hello'.text)
+@bot.message_handler(commands=["text"])
+def cmd_start(message):
+    bot.send_message(message.chat.id, "Ой, ну Привет...")
+    dbworker.set_state(message.chat.id, config.States.S_ENTER_NAME.value)
+
 
 if __name__ == '__main__':
     bot.polling(none_stop=True)
