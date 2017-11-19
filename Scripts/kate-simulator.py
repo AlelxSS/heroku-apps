a81dff7b301d2b72795fb258e3868ad0fcfe8a4e
AlelxSS <titorovichva@gmail.com>
2017-11-19 10:48:36 +0300

    kate cimulator

--- Scripts/kate-simulator.py
+++ Scripts/kate-simulator.py
@@ -1,4 +1,5 @@
 import telebot
+import random
 
 token = '497028477:AAERLo-AfA-NwqvcngZtuM5BvUq-wxVCLOM'
 
@@ -7,8 +8,9 @@ bot = telebot.TeleBot(token)
 
 @bot.message_handler(content_types=["text"])
 def answer_all_messages(message): 
-	if message.text.lower().find('привет')>=0: 
-		bot.send_message(message.chat.id, 'И тебе привет')
+	if (message.text.lower().find('привет')>=0) or (message.text.lower().find('здаров')>=0): 
+		foo1 = ['И тебе привет', 'Аггаа', 'Ой, ну привеет...']
+		bot.send_message(message.chat.id, random.choice(foo1))
 
 if __name__ == '__main__':
     bot.polling(none_stop=True)
